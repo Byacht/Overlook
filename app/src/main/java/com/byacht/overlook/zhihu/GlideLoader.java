a8660d32765ceaf6d47527bf64dd35c75946ee0f
HT <374984629@qq.com>
2017-08-12 13:28:34 +0800

    improve first edition

---
+++ app/src/main/java/com/byacht/overlook/zhihu/GlideLoader.java
@@ -0,0 +1,8 @@
+package com.byacht.overlook.zhihu;
+
+/**
+ * Created by dn on 2017/8/10.
+ */
+
+public class GlideLoader {
+}
---
+++ app/src/main/java/com/byacht/overlook/zhihu/entity/ZhihuHot.java
@@ -0,0 +1,8 @@
+package com.byacht.overlook.zhihu.entity;
+
+/**
+ * Created by dn on 2017/8/10.
+ */
+
+public class ZhihuHot {
+}
